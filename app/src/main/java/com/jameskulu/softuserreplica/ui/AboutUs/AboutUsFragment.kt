1f0cde7e6e635c1f0b1787fb777b9016edb93fc4
James Kulu <jameskulu55@gmail.com>
2021-01-14 00:29:13 +0545

    Added web view

--- app/src/main/java/com/jameskulu/softuserreplica/ui/AboutUs/AboutUsFragment.kt
+++ app/src/main/java/com/jameskulu/softuserreplica/ui/AboutUs/AboutUsFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.jameskulu.softuserreplica.R
 
 class AboutUsFragment : Fragment() {
 
-    private lateinit var aboutUsViewModel: AboutUsViewModel
+    private lateinit var webView: WebView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -17,6 +19,21 @@ class AboutUsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_aboutus, container, false)
+
+
+        webView = root.findViewById(R.id.webview)
+        webView.settings.setJavaScriptEnabled(true)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl("https://softwarica.edu.np/")
+
         return root
     }
 }
--- app/src/main/java/com/jameskulu/softuserreplica/ui/AddStudent/AddStudentFragment.kt
+++ app/src/main/java/com/jameskulu/softuserreplica/ui/AddStudent/AddStudentFragment.kt
@@ -1,20 +1,108 @@
 package com.jameskulu.softuserreplica.ui.AddStudent
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.jameskulu.softuserreplica.MainActivity
 import com.jameskulu.softuserreplica.R
+import com.jameskulu.softuserreplica.model.User
 
 class AddStudentFragment : Fragment() {
+
+    private var users = ArrayList<User>()
+    private var gender = ""
+
+    private lateinit var etFullName : EditText
+    private lateinit var etAge : EditText
+    private lateinit var etAddress : EditText
+    private lateinit var rdoMale : RadioButton
+    private lateinit var rdoFemale : RadioButton
+    private lateinit var rdoOthers : RadioButton
+    private lateinit var btnSave : Button
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
         val root = inflater.inflate(R.layout.fragment_addstudent, container, false)
+
+        etFullName = root.findViewById(R.id.etFullName)
+        etAge = root.findViewById(R.id.etAge)
+        etAddress = root.findViewById(R.id.etAddress)
+        rdoMale = root.findViewById(R.id.rdoMale)
+        rdoFemale = root.findViewById(R.id.rdoFemale)
+        rdoOthers = root.findViewById(R.id.rdoOthers)
+        btnSave = root.findViewById(R.id.btnSave)
+
+
+        btnSave.setOnClickListener {
+            if(validation()) {
+
+                when {
+                    rdoMale.isChecked -> {
+                        gender = "Male"
+                    }
+                    rdoFemale.isChecked -> {
+                        gender = "Female"
+                    }
+                    rdoOthers.isChecked -> {
+                        gender = "Others"
+                    }
+                }
+
+
+                users.add(
+                    User(
+                        1,
+                        etFullName.text.toString(),
+                        etAge.text.toString().toInt(),
+                        etAddress.text.toString(),
+                        gender,
+                        "ss",
+                    )
+                )
+
+
+
+
+                Toast.makeText(context, "Student saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         return root
+    }
+
+    private fun validation():Boolean{
+        var isEmpty:Boolean = true
+
+        when {
+            TextUtils.isEmpty(etFullName.text) -> {
+                etFullName.error = "Enter full name."
+                etFullName.requestFocus()
+                isEmpty = false
+            }
+            TextUtils.isEmpty(etAge.text) -> {
+                etAge.error = "Enter age."
+                etAge.requestFocus()
+                isEmpty = false
+            }
+            TextUtils.isEmpty(etAddress.text) -> {
+                etAddress.error = "Enter address."
+                etAddress.requestFocus()
+                isEmpty = false
+            }
+        }
+        return isEmpty
     }
 }
